abd455995b189e817b84b12a834e5f8f43a185dc
forgineer <blake.phillips86@gmail.com>
2022-12-17 12:45:09 -0600

    Code and README

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # pyNorris
-A Chuck Norris joke teller written in Python.
+This is an older project that I completed in 2019 while a co-worker and I were learning Python together for the first time, and after completing a set of online tutorials we decided to test ourselves with a final project before moving on to another language. For this final project, I decided to [re-use this idea](https://github.com/forgineer/ChuckNorrisIO) of creating a front-end application that would retrieve Chuck Norris jokes from the [Chuck Norris IO API](https://api.chucknorris.io/). The set of online tutorials we followed can be found at [TutorialsPoint.com](https://www.tutorialspoint.com/python3/index.htm).
+
+## How it Works...
+Running the `pyNorris.py` module application will present the user with a very simple interface that includes a background picture of Chuck Norris with a speech bubble. The speech bubble will contain some default text encouraging the user to get a new joke. In the bottom left-hand corner of the application are a drop-down and a button.
+
+<p align="center">
+  <img src="/images/pyNorris_demo1.png">
+</p>
+
+The drop-down contains a list of available joke categories that the user can select for a specific type of joke they would like to get from the [Chuck Norris IO API](https://api.chucknorris.io/). The "Get Joke!" button will retrieve a new joke by calling the API and displaying it in the speech bubble.
+
+<p align="center">
+  <img src="/images/pyNorris_demo2.png">
+</p>
+
+After retrieving the joke from the API, before displaying the joke in the speech bubble, the joke will be stored in a local SQLite database including some additional metadata from the API and a timestamp when the joke was stored.
+
+<p align="center">
+  <img src="/images/pyNorris_demo3.png">
+</p>
--- pyNorris.py
+++ pyNorris.py
@@ -1,9 +1,10 @@
 """
-   Program Name:  PyNorris.py
+   Program Name:  pyNorris.py
    Author:        Blake Phillips
-   Description:   Chuck Norris Joke Generator
+   Description:   Chuck Norris Joke Teller
 
-   Utilizes the chucknorris.io API for retrieving random Chuck Norris "facts" jokes or jokes based on a set of categories given by the API
+   Utilizes the chucknorris.io API for retrieving Chuck Norris "facts" or jokes at random or based on a given
+   category provided by the API.
 
    https://api.chucknorris.io/jokes/random
    https://api.chucknorris.io/jokes/categories
@@ -17,19 +18,22 @@
    "id":"hNut57c3Q7qPmIGi57LKog",
    "updated_at":"2020-01-05 13:42:21.179347",
    "url":"https://api.chucknorris.io/jokes/hNut57c3Q7qPmIGi57LKog",
-   "value":"Whitney Houston was recently found dead in a Hotel room with drugs all around her body. She overdosed once she found out Chuck Norris was actually her father."
+   "value":"Whitney Houston was recently found dead in a Hotel room with drugs all around her body. She overdosed once
+   she found out Chuck Norris was actually her father."
    }'
 """
 from tkinter import *
-from PIL import Image, ImageTk   # pip install pillow
-#import Image, ImageTk
+from PIL import Image, ImageTk  # pip install Pillow
 import http.client
 import json
+import logging
 import sqlite3
 import time
 
+# Set log level for debugging
+# logging.basicConfig(level=logging.DEBUG)
 
-# Define Global Variables
+# Define Base URL and Paths for chucknorris.io API
 baseURL = "api.chucknorris.io"
 pathRandom = "/jokes/random"
 pathCategories = "/jokes/categories"
@@ -38,115 +42,141 @@ pathCategories = "/jokes/categories"
 apiConnect = http.client.HTTPSConnection(baseURL)
 
 
-# Define Program Functions
 def getCategories():
-   apiConnect.request("GET", pathCategories)
-   respCategories = apiConnect.getresponse()
+    logging.info("Retrieving joke categories...")
 
-   if(respCategories.status == 200):
-      jsonCategories = respCategories.read()
-      catList = json.loads(jsonCategories)
-      catList.insert(0, 'random')   # Include 'random' as the first option in the list
+    apiConnect.request("GET", pathCategories)
+    respCategories = apiConnect.getresponse()
 
-      #print(categories)
-      #for category in categories: # Traversal of List sequence
-      #   print(category)
-      return catList
-   else: # If no connection, make 'random' the only choice in the list
-      catList = ['random']
-      return catList
+    if respCategories.status == 200:
+        jsonCategories = respCategories.read()
+        catList = json.loads(jsonCategories)
+        catList.insert(0, 'random')  # Include 'random' as the first option in the list
+
+        logging.debug(f'List of Categories: {catList}')
+
+        return catList
+    else:  # If no connection, make 'random' the only choice in the list
+        logging.info("Unable to retrieve categories, random only...")
+        catList = ['random']
+        return catList
+
 
 def getJoke():
-   if(optionVar.get() != 'random'):
-      getCategoryStr = pathRandom + "?category=" + optionVar.get()
-      apiConnect.request("GET", getCategoryStr)
-      respJoke = apiConnect.getresponse()
-   else:
-      apiConnect.request("GET", pathRandom)
-      respJoke = apiConnect.getresponse()
+    logging.info(f'Getting next {optionVar.get()} joke...')
 
-   #print(optionVar.get())
-   #apiConnect.request("GET", pathRandom)
-   #respJoke = apiConnect.getresponse()
-   
-   if(respJoke.status == 200):
-      #print(respJoke)
-      jokeJSON = respJoke.read()
-      #print(jokeJSON)
-      jokeList = json.loads(jokeJSON)
-      #print(jokeList["value"])
-      jokeText.set(jokeList["value"])
+    if optionVar.get() != 'random':
+        getCategoryStr = pathRandom + "?category=" + optionVar.get()
+        apiConnect.request("GET", getCategoryStr)
+        respJoke = apiConnect.getresponse()
+    else:
+        apiConnect.request("GET", pathRandom)
+        respJoke = apiConnect.getresponse()
 
-      # Store the joke in local DB (PyNorris.db)
-      jokeId = jokeList["id"]
-      jokeCreateDtTm = jokeList["created_at"]
-      jokeValue = jokeList["value"]
+    if respJoke.status == 200:
+        jokeJSON = respJoke.read()
+        logging.debug(f'Response: {jokeJSON}')
 
-      storeFlg = storeJoke(jokeId, jokeCreateDtTm, jokeValue)
-   else:
-      jokeText.set("For some reason I wasn't able to get a new joke. Try again in a minute")
+        jokeDict = json.loads(jokeJSON)  # Load JSON as dictionary
+        logging.debug(f'JSON list: {jokeDict}')
 
-def storeJoke(pId, pDate, pValue):
-   dbConnect = sqlite3.connect('PyNorris.db')
-   dbCursor = dbConnect.cursor()
+        jokeText.set(jokeDict["value"])  # Set bubble text
 
-   sqlStr = "SELECT ID FROM JOKES WHERE ID = '" + pId + "'"
-   dbCursor.execute(sqlStr)
-   row_count = dbCursor.rowcount
+        # Store the joke in local DB (PyNorris.db)
+        jokeId = jokeDict["id"]
+        jokeCreateDtTm = jokeDict["created_at"]
+        jokeValue = jokeDict["value"]
 
-   if(row_count <= 0):
-      localtime = time.localtime(time.time())
+        if not jokeDict["categories"]:
+            jokeCategory = "random"
+        else:
+            jokeCategory = jokeDict["categories"][0]
 
-      year = str(localtime[0])
-      month = str(localtime[1])
-      day = str(localtime[2])
-      hour = str(localtime[3])
-      minute = str(localtime[4])
-      second = str(localtime[5])
+        logging.debug(f'id: {jokeId}')
+        logging.debug(f'created_at: {jokeCreateDtTm}')
+        logging.debug(f'value: {jokeValue}')
+        logging.debug(f'categories: {jokeCategory}')
 
-      stored_dt_tm = year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second
-      #print(stored_dt_tm)
-
-      sqlStr = "INSERT INTO JOKES (ID, CREATED_AT, VALUE, STORED_DT_TM) VALUES(?, ?, ?, ?)"
-      dbCursor.execute(sqlStr, (pId, pDate, pValue, stored_dt_tm))
-      dbConnect.commit()
-      dbConnect.close()
-   
-   return 1 # true
+        storeJoke(jokeId, jokeCreateDtTm, jokeValue, jokeCategory)
+    else:
+        jokeText.set("For some reason I wasn't able to get a new joke. Try again later.")
 
 
-# *** Main Program Start ***
+def storeJoke(pId, pDate, pValue, pCategory):
+    dbConnect = sqlite3.connect('pyNorris.db')  # Creates DB if not exists
+    dbCursor = dbConnect.cursor()
+    jokeExists = False
 
+    try:
+        sqlStr = "SELECT id FROM jokes WHERE id = '" + pId + "'"
+        dbCursor.execute(sqlStr)
+
+        if dbCursor.rowcount > 0:
+            jokeExists = True
+    except:
+        logging.exception("Failed to check if joke is already stored. The table probably did not exist. Creating...")
+
+        # Create JOKES table if not exist
+        sqlStr = """
+            CREATE TABLE jokes(
+                id TEXT,
+                created_at TEXT,
+                value TEXT,
+                category TEXT,
+                stored_dt_tm TEXT
+            )
+        """
+        dbCursor.execute(sqlStr)
+        dbConnect.commit()
+
+    if not jokeExists:
+        localtime = time.localtime(time.time())
+
+        year = str(localtime[0])
+        month = str(localtime[1])
+        day = str(localtime[2])
+        hour = str(localtime[3])
+        minute = str(localtime[4])
+        second = str(localtime[5]).rjust(2, '0')
+
+        stored_dt_tm = year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second
+        logging.debug(f'Joke will be stored at {stored_dt_tm}')
+
+        sqlStr = "INSERT INTO jokes (id, created_at, value, category, stored_dt_tm) VALUES(?, ?, ?, ?, ?)"
+        dbCursor.execute(sqlStr, (pId, pDate, pValue, pCategory, stored_dt_tm))
+        dbConnect.commit()
+        dbConnect.close()
+
+
+# Main
 # Setup root window
 pyNorrisWindow = Tk()
-pyNorrisWindow.title("PyNorris")
-pyNorrisWindow.geometry("784x442")
+pyNorrisWindow.title("pyNorris")
+pyNorrisWindow.geometry("784x442")  # Size of the background image
 pyNorrisWindow.resizable(width=False, height=False)
 
 # Set label for background image of Chuck Norris
-img = Image.open("chuck_norris_bubble.jpg")
+img = Image.open("images/chuck_norris_bubble.jpg")
 norrisImg = ImageTk.PhotoImage(img)
 bgLabel = Label(pyNorrisWindow, image=norrisImg)
 bgLabel.place(x=0, y=0)
 
 # Set drop-down list of category options for Chuck Norris jokes
 optionVar = StringVar()
-optionVar.set('random') # Set random by default
+optionVar.set('random')  # Set random by default
 categories = getCategories()
 categoryOptions = OptionMenu(pyNorrisWindow, optionVar, *categories)
-categoryOptions.place(x=5, y=405)
+categoryOptions.place(x=5, y=400)
 
 # Set button for getting a new Chuck Norris joke and displaying it
 jokeButton = Button(pyNorrisWindow, text="Get Joke!", command=getJoke)
-jokeButton.place(x=105, y=408.5)
+jokeButton.place(x=105, y=400)
 
 # Set Joke text label over image word bubble
 jokeText = StringVar()
 jokeLabel = Label(pyNorrisWindow, textvariable=jokeText, bg="yellow", wraplength=180)
 jokeText.set("Click the 'Get Joke!' button for a Chuck Norris fact!")
-jokeLabel.place(x=580, y=180)
+jokeLabel.place(x=580, y=221)
 
 # Start up the PyNorris GUI program!
 pyNorrisWindow.mainloop()
-
-# *** Main Program End ***
